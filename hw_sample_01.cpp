91d36f2b1c853a59b45397e77dcb730f44bf9383
cvh upci <cvh.upci@gmail.com>
2015-02-03 22:49:01 -0600

    homework solved

---
+++ hw_sample_01.cpp
@@ -0,0 +1,26 @@
+Author:   Juan Pueblo
+Date:     today
+Version:  1.0
+
+/*
+  Homework 01
+  this assignment try ...
+*/
+
+#include <iostream>
+
+int main ()
+{
+  double x=112.56,y=568.1,z=225.259;
+  double r;
+
+  r = x+y+z;
+
+  std::cout<<" x: "<<x<<std::endl;
+  std::cout<<" y: "<<y<<std::endl;
+  std::cout<<" z: "<<z<<std::endl;
+
+  std::cout<<" (x+y+x)/3 = "<< r <<std::endl;
+
+  return 0;
+}
