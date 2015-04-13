ff3fab855f21d067dfd722e34a0641d0d79d09a9
sunandmoon2 <noaromeoandrus12@gmail.com>
2015-04-13 11:34:37 -0500

    HOMEWORK 01
    Este codigo muestra mensaje  Hello world desde la salida estandar

--- hw_01_id_22.cpp
+++ hw_01_id_22.cpp
@@ -1,19 +1,18 @@
-// Author:
-// Date:
-// Version:
+// Author: Noe Romero andrade
+// Date: 13-april-2015
+// Version: 01
 
 /*
-Homework DD
-this assignment  ...
+Homework 01
+this assignment  Muestra desde la salida estandar el mensaje Hello wolrd y soy noe
 */
 
 #include <iostream>
 
 int main ()
 {
-  // TODO your code here!!!!
 
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+  std::cout<<"Hello world!!" <<std::endl;//se usa el comando cout para inprimir en la salida estandar
+  std::cout<<"Soy Noe" <<std::endl;//se usa el endl para salto de linea
+  return 0;//valor de retorno
 }
